2f8b612309adb7cb7a53ce3dc1aa66f19ae6e0d2
Trust Onyemachi <85189857+code-with-onye@users.noreply.github.com>
2021-09-13 14:25:42 +0100

    Edit profile

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,7 +3,7 @@ function Avatar({ url, className }) {
     <img
       loading="lazy"
       className={`h-10 transition duration-150 transform rounded-full cursor-pointer hover:scale-110 ${className}`}
-      src="https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTUxODYzMjQzNTc5NTk4Mjk3/barron-trump_gettyimages-877137998.jpg"
+      src="https://avatars.githubusercontent.com/u/85189857?s=64&v=4"
       alt="profile pic"
     />
   );
