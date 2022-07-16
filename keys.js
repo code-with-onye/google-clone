aa3dbcbe902273bb1daf4c4b5b8e085dfa65f275
Trust Onyemachi <85189857+code-with-onye@users.noreply.github.com>
2022-07-16 15:35:01 -0700

    Delete Api key

--- keys.js
+++
@@ -1,5 +0,0 @@
-//Api key - https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = "AIzaSyDYYZ-kNQvUGpDwKCf69Ve7k5mv8VsmcmI";
-//Context Key - https://cse.google.com/cse/create/new
-
-export const CONTEXT_KEY = "097bce0cb39fdbbec";
